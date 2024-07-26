d5f527693b892931db7c21eac71d68ff1e9f1889
Onni Virtanen <ac7545@student.jamk.fi>
2024-07-26 10:54:14 +0300

    vector add works

--- addition/vector_add.cu
+++ addition/vector_add.cu
@@ -1,4 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <assert.h>
+#include <cuda.h>
+#include <cuda_runtime.h>
+
 #define N 10000000
+#define MAX_ERR 1e-6
 
 __global__ void vector_add(float* out, float* a, float* b, int n) {
     for (int i = 0; i < n; i++) {
@@ -7,18 +15,54 @@ __global__ void vector_add(float* out, float* a, float* b, int n) {
 }
 
 int main() {
-    float* a, * b, * out;
+    float* a;
+    float* b;
+    float* out;
 
-    // Allocate memory
+    float* d_a;
+    float* d_b;
+    float* d_out;
+
+    // Allocate host memory
     a = (float*)malloc(sizeof(float) * N);
     b = (float*)malloc(sizeof(float) * N);
     out = (float*)malloc(sizeof(float) * N);
 
-    // Initialize array
+    // Initialize host arrays
     for (int i = 0; i < N; i++) {
-        a[i] = 1.0f; b[i] = 2.0f;
+        a[i] = 1.0f; 
+        b[i] = 2.0f;
     }
 
+    // Allocate device memory
+    cudaMalloc((void**)&d_a, sizeof(float) * N);
+    cudaMalloc((void**)&d_b, sizeof(float) * N);
+    cudaMalloc((void**)&d_out, sizeof(float) * N);
+
+    // Transfer input data from host to device memory
+    cudaMemcpy(d_a, a, sizeof(float) * N, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, b, sizeof(float) * N, cudaMemcpyHostToDevice);
+
     // Kernel call
-    vector_add<<<1,1>>>(out, a, b, N);
+    vector_add<<<1,1>>>(d_out, d_a, d_b, N);
+
+    // Transfer data from device to host memory
+    cudaMemcpy(out, d_out, sizeof(float) * N, cudaMemcpyDeviceToHost);
+
+    // Verification
+    for(int i = 0; i < N; i++){
+        assert(fabs(out[i] - a[i] - b[i]) < MAX_ERR);
+    }
+    printf("out[0] = %f\n", out[0]);
+    printf("PASSED\n");
+
+    // Deallocate device memory
+    cudaFree(d_a);
+    cudaFree(d_b);
+    cudaFree(d_out);
+
+    // Deallocate host memory
+    free(a);
+    free(b);
+    free(out);
 }
